50dc7995937a31b3363cfed47b09e3937188559e
Brad_illig <illigbrad@gmail.com>
2017-04-16 14:03:56 -0700

    init commit

---
+++ DiscordBot/MyBot.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    class MyBot
+    {
+        DiscordClient discord;
+
+        public MyBot()
+        {
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("MzAzMjY2MTY4MDMzMTE2MTYz.C9VrTg.GItnjGeV-JRpg3qG2OKC9JpDqTw", TokenType.Bot);
+            });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -10,6 +10,7 @@ namespace DiscordBot
     {
         static void Main(string[] args)
         {
+            MyBot bot = new MyBot();
         }
     }
 }
