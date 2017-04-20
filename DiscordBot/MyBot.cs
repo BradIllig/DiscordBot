84c13c052203a36bd9dfde78e844534b9ed51b8e
Brad_illig <illigbrad@gmail.com>
2017-04-19 20:27:34 -0700

    added Listen feature

--- DiscordBot/MyBot.cs
+++ DiscordBot/MyBot.cs
@@ -15,16 +15,15 @@ namespace DiscordBot
         DiscordClient discord;
         CommandService commands;
 
-        string[] Memes;
         Random rand = new Random();
 
         string directory = @".\Memes\";
         List<string> memeImages = new List<string>();
+        
 
 
-    public MyBot()
+        public MyBot()
         {
-            
             discord = new DiscordClient(x =>
             {
                 x.LogLevel = LogSeverity.Info;
@@ -37,11 +36,12 @@ namespace DiscordBot
                 x.AllowMentionPrefix = true;
             });
 
+            
+
             commands = discord.GetService<CommandService>();
 
-            RegisterMemeCommand();
-            RegisterResponseCommand();
-
+            ListenForCommands();
+            
             discord.ExecuteAndWait(async () =>
             {
                 await discord.Connect("MzAzMjY2MTY4MDMzMTE2MTYz.C9VrTg.GItnjGeV-JRpg3qG2OKC9JpDqTw", TokenType.Bot);
@@ -49,35 +49,46 @@ namespace DiscordBot
 
         }
 
-        private void RegisterMemeCommand()
+        private void ListenForCommands()
         {
+
             foreach (string myMeme in Directory.GetFiles(directory, "*.jpg", SearchOption.AllDirectories))
             {
                 memeImages.Add(myMeme);
             }
 
-            commands.CreateCommand("freshmemepls")
-                .Do(async (e) =>
+
+                discord.MessageReceived += async (s, e) =>
+            {
+                if (!e.Message.IsAuthor)
                 {
-                    int randMeme = rand.Next(memeImages.ToArray().Length);
-                    string memeToPost = memeImages[randMeme];
-                    await e.Channel.SendFile(memeToPost);
-                    await e.Channel.SendMessage("*Send your memes to* **Illigbrad@hotmail.com**");
-                });
+                    //await e.Channel.SendMessage(e.Message.Text);
+                    if (e.Message.Text.Contains("$"))
+                    {
+                        if (e.Message.Text.Contains("meme"))
+                        {
+                            int randMeme = rand.Next(memeImages.ToArray().Length);
+                            string memeToPost = memeImages[randMeme];
+                            await e.Channel.SendFile(memeToPost);
+                        }
+                        else
+                        {
+                            await e.Channel.SendMessage("fuk u");
+                        }
+                    }
+                    if (e.Message.Text.Contains("/tts"))
+                    {
+                        await e.Channel.SendMessage("fuk u");
+                    }
+                }
+            };
         }
 
-        private void RegisterResponseCommand()
-        {
-            commands.CreateCommand("hello")
-                .Do(async (e) =>
-                {
-                    await e.Channel.SendMessage("fuk u");
-                });
-        }
 
         private void Log(object sender, LogMessageEventArgs e)
         {
             Console.WriteLine(e.Message);
         }
     }
+    
 }
