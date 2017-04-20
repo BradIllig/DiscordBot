6b2adf353aba80d94ae028c0ce03cc93cb0e0b60
Brad_illig <illigbrad@gmail.com>
2017-04-19 23:19:11 -0700

    simplified marine

--- DiscordBot/MyBot.cs
+++ DiscordBot/MyBot.cs
@@ -20,17 +20,20 @@ namespace DiscordBot
         string directory = @".\Memes\";
         List<string> memeImages = new List<string>();
 
-        string marine1 = "What the fuck did you just fucking say about me you little bitch I’ll have you know I graduated top of my class in the Navy Seals and I’ve been involved in numerous secret raids";
-        string marine2 = "on Al-Quaeda and I have over 300 confirmed kills I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces You are nothing to me but just another target";
-        string marine3 = "I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth mark my fucking words You think you can get away with saying that shit to me";
-        string marine4 = "over the Internet Think again fucker As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm";
-        string marine5 = "maggot The storm that wipes out the pathetic little thing you call your life You’re fucking dead kid I can be anywhere anytime and I can kill you in over seven hundred ways and";
-        string marine6 = "that’s just with my bare hands Not only am I extensively trained in unarmed combat but I have access to the entire arsenal of the United States Marine Corps and I will use it to its";
-        string marine7 = "full extent to wipe your miserable ass off the face of the continent you little shit If only you could have known what unholy retribution your little “clever” comment was about to"; 
-        string marine8 = "bring down upon you maybe you would have held your fucking tongue But you couldn’t you didn’t and now you’re paying the price you goddamn idiot I will shit fury all over you";
-        string marine9 = "and you will drown in it You’re fucking dead, kiddo.";
+        String[] marineText = {
+        "What the fuck did you just fucking say about me you little bitch I’ll have you know I graduated top of my class in the Navy Seals and I’ve been involved in numerous secret raids",
+        "on Al-Quaeda and I have over 300 confirmed kills I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces You are nothing to me but just another target",
+        "I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth mark my fucking words You think you can get away with saying that shit to me",
+        "over the Internet Think again fucker As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm",
+        "maggot The storm that wipes out the pathetic little thing you call your life You’re fucking dead kid I can be anywhere anytime and I can kill you in over seven hundred ways and",
+        "that’s just with my bare hands Not only am I extensively trained in unarmed combat but I have access to the entire arsenal of the United States Marine Corps and I will use it to its",
+        "full extent to wipe your miserable ass off the face of the continent you little shit If only you could have known what unholy retribution your little “clever” comment was about to",
+        "bring down upon you maybe you would have held your fucking tongue But you couldn’t you didn’t and now you’re paying the price you goddamn idiot I will shit fury all over you",
+        "and you will drown in it You’re fucking dead, kiddo."
+        };
 
-        public MyBot()
+
+    public MyBot()
         {
             discord = new DiscordClient(x =>
             {
@@ -87,16 +90,11 @@ namespace DiscordBot
                     }
                     if (e.Message.Text.ToLower().Contains("re"))
                     {
-                        await e.Channel.SendTTSMessage(marine1);
-                        await e.Channel.SendTTSMessage(marine2);
-                        await e.Channel.SendTTSMessage(marine3);
-                        await e.Channel.SendTTSMessage(marine4);
-                        await e.Channel.SendTTSMessage(marine5);
-                        await e.Channel.SendTTSMessage(marine6);
-                        await e.Channel.SendTTSMessage(marine7);
-                        await e.Channel.SendTTSMessage(marine8);
-                        await e.Channel.SendTTSMessage(marine9);
+                        foreach (string marine in marineText)
+                        {
+                            await e.Channel.SendTTSMessage(marine);
                         }
+                    }
                 }
             };
         }
